b0a2a88a5c523f3f46542600eaf2385f8f7c0ea5
Yuanuanuan <asdll6618@gmail.com>
2023-11-12 12:15:34 +0800

    feat: home scroll animation

--- js/homePage.js
+++ js/homePage.js
@@ -3,7 +3,6 @@ const coverDoor = document.querySelector(".cover-door");
 const coverImage = document.querySelector(".cover-image");
 
 const handleScroll = () => {
-  console.log(window.scrollY);
   if (window.scrollY <= 400) {
     let scale = window.scrollY === 0 ? 1 : 1 + window.scrollY / 100;
     let imageScale = window.scrollY === 0 ? 1 : 1 + window.scrollY / 1000;
@@ -33,8 +32,56 @@ coverTextAnimation
   })
   .to(coverTextH1, { opacity: 1, duration: 0.3 });
 
+const aboutUsTitle = document.querySelector(
+  ".about-us-wrapper .first-part .title"
+);
+const aboutUsDescribe = document.querySelector(
+  ".about-us-wrapper .first-part .describe"
+);
+const aboutUsMoreBtn = document.querySelector(
+  ".about-us-wrapper .first-part .more-btn"
+);
+
 const aboutUsAnimation = gsap.timeline({ paused: true });
-aboutUsAnimation.to();
+
+aboutUsAnimation
+  .from(aboutUsTitle, { duration: 1.5, y: 50, opacity: 0, ease: "power4.out" })
+  .from(
+    aboutUsDescribe,
+    { duration: 1.5, y: 80, opacity: 0, ease: "power4.out" },
+    "<0.3"
+  )
+  .from(aboutUsMoreBtn, { opacity: 0 }, "<0.3");
+
+gsap.to(aboutUsAnimation, {
+  scrollTrigger: {
+    trigger: "section.about-us",
+    start: "top 40%",
+    end: "bottom center",
+    onEnter: () => aboutUsAnimation.play(),
+    onEnterBack: () => aboutUsAnimation.play(),
+  },
+});
+
+const serviceItems = document.querySelectorAll(
+  "section.service .content .middle .item"
+);
 
 const serviceAnimation = gsap.timeline({ paused: true });
-serviceAnimation.to();
+serviceAnimation.from(serviceItems, {
+  duration: 1,
+  y: 60,
+  opacity: 0,
+  ease: "power4.out",
+  stagger: .5,
+});
+
+gsap.to(serviceAnimation, {
+  scrollTrigger: {
+    trigger: "section.service",
+    start: "top 30%",
+    end: "bottom center",
+    onEnter: () => serviceAnimation.play(),
+    onEnterBack: () => serviceAnimation.play(),
+  },
+});
